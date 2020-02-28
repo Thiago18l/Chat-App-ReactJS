d74ee089a12f280215b1e9f225b4ba8952fef2f7
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-02-28 02:04:10 -0300

    Adicionando métodos para a Gestão dos Users

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,21 +10,43 @@ const router = require('./router'); // Importando a rota...
 const App = express();
 const server = http.createServer(App);
 const io = socketio(server);
+App.use(router);
 
+io.on('connect', (socket) => {
+    socket.on('join', ({ name, room }, callback) => {
+      const { error, user } = addUser({ id: socket.id, name, room });
+  
+      if(error) return callback(error);
+  
+      socket.join(user.room);
+  
+      socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+      socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+  
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+  
+      callback();
+    });
 
-io.on('connection', (socket) => {
-    console.log('New connection');
+    socket.on('sendMessage', (message, callback)=> {    
+        const user = getUser(socket.id);
+        
+        io.to(user.room).emit('message', { user: user.name, text: message });
+
+        callback();
+
+    });
     
     socket.on('disconnect', () => { // sem parametro na função por que o usuário vai sair.
         console.log('User had left');
     })
 })
 
-App.use(router);
 
 
-server.listen(PORT), () => {
+
+server.listen(PORT, () => {
     console.log(`Server has Started on Port ${PORT}`);
-};
+});
 
 
