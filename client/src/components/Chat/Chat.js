cd83e04607fac66442c5228154db4285f75cc74e
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-02-28 00:49:04 -0300

    adicionando react hooks ao projeto

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './Chat.css';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+let socket;
 
 // CSS
-import './Chat.css';
 
-const Chat = () =>{
+
+const Chat = ( {location} ) =>{
+    const [name, setName] = useState("");
+    const [room, setRoom] = useState("");
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(()=> {
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+        
+        socket.emit('join', { name, room }); // is equal to say name: name, room: room
+
+    }, [ENDPOINT, location.search]);
     return (
         <h1>Chat</h1>
     );
