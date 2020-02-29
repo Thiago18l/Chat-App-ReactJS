820cbb60dce100d934f8dc53536a212ebf7d3887
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-02-29 19:07:52 -0300

    Alterações na lógica de enviar mensagens

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -5,10 +5,10 @@ import './Messages.css';
 
 import Message from './Message/Message'
 
-const Messages = ( { messages, name } ) => (
-    <ScrollToBottom>
-        {messages.map((message, index) => <div key={index}><Message message={message} name={name}/></div>)}
+const Messages = ({ messages, name }) => (
+    <ScrollToBottom className="messages">
+      {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
     </ScrollToBottom>
-)
+  );
 
 export default Messages;
